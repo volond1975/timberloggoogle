61f98abd44446584dec2b24119d7fe95d305bcdb
volond1975 <volond@gmail.com>
2018-08-10 16:05:35 +0300

    Update touchEvent.js

--- touchEvent.js
+++ touchEvent.js
@@ -1,1 +1,3 @@
-
+//http://youon.ru/%D0%90%D0%BD%D0%B4%D1%80%D0%BE%D0%B8%D0%B4/%D0%A0%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0/touch-sobytiya-na-javascript-multitach-realizatsiya
+//https://css-tricks.com/the-javascript-behind-touch-friendly-sliders/
+//https://artemdemo.me/blog/%D0%BD%D0%B0%D0%B6%D0%B0%D1%82%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B5-%D1%81%D0%BC%D0%B0%D1%80%D1%82%D1%84%D0%BE%D0%BD%D0%B0-tap-event/
